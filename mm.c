84d04e0d811ac4e16b0c33f60ad3b0cb0e2df19f
Sharif Velasquez <sharifvelasquez@gmail.com>
2026-07-01 08:20:58 -0700

    chore: initial scaffolding

---
+++ mm.c
@@ -0,0 +1,17 @@
+#include "mm.h"
+
+int mm_init() {
+    return 0;
+}
+
+void* mm_malloc(size_t size) {
+    return 0;
+}
+
+void mm_free(void* ptr) {
+
+}
+
+void* mm_realloc(void* ptr, size_t size) {
+    return 0;
+}
